3a7625b1cade8047d83ae87cd877f7e1eee80cfd
harshit-malik7 <harshit4731@gmail.com>
2023-08-31 16:37:36 -0600

    visual changes

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src="https://www.ualberta.ca/women-in-scholarship-engineering-science-technology/media-library/images/logos/copy-of-faculty-of-engineering.jpg"></img>
-        <button>About me</button>
-        <button>Contact me</button>
+        <div className='button-container'>
+          <button className='btn btn-primary btn-lg mx-3 px-5 py-3 mt-2' onClick={}>About me</button>
+          <button className='btn btn-primary btn-lg mx-3 px-5 py-3 mt-2' onClick={}>Contact me</button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
