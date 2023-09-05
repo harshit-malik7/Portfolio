4ef83377dfae52f8e80af8b21b90d8f024b63be1
harshit-malik7 <harshit4731@gmail.com>
2023-09-05 14:01:54 -0600

    added contact on bottom of the page

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -25,8 +25,11 @@ const Home = () => {
           <button className='btn btn-primary btn-lg mx-3 px-5 py-3 mt-2' onClick={scrolltoBottom}>Contact me</button>
         </div>
       </header>
-      <div ref={bottomElement}>
-        <p>Contact me</p>
+      <div ref={bottomElement} className='contact'>
+        <h1 style={{color:'pink'}}>Contact Me</h1>
+        <a href="https://www.linkedin.com/in/harshit-malik-273108224/"><img className="linkedin" src="https://cdn-icons-png.flaticon.com/512/174/174857.png"></img></a>
+        <a href="https://github.com/harshit-malik7"><img className="linkedin" src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></img></a>
+        <a href="mailto:harshit4731@gmail.com"><img className="linkedin" src="https://cdn-icons-png.flaticon.com/512/732/732200.png"></img></a>
       </div>    
     </div>
   )
