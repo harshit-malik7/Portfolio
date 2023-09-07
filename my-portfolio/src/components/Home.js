d963a14876a5c1044c0eecdbe0dbd02305216d98
harshit-malik7 <harshit4731@gmail.com>
2023-09-07 14:02:41 -0600

    design changes

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -20,6 +20,7 @@ const Home = () => {
 
     const sun = useRef(new Image());
     const earth = useRef(new Image());
+    const mars = useRef(new Image());
     const canvasref=useRef(null);
     useEffect(()=>{
       let requestID;
@@ -27,19 +28,22 @@ const Home = () => {
       
       const canvas=canvasref.current;
       const ctx=canvas.getContext("2d");
-      sun.current.src = "https://upload.wikimedia.org/wikipedia/commons/7/78/F-type_subgiant_star_in_Celestia_03.png";
+      // sun.current.src = "https://upload.wikimedia.org/wikipedia/commons/7/78/F-type_subgiant_star_in_Celestia_03.png";
+      sun.current.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ88IDEtSSTQUnYes_xdF23woS-Wu0iSmIRVg&usqp=CAU";
       earth.current.src = "https://i.etsystatic.com/28921914/r/il/6012cc/3477638374/il_fullxfull.3477638374_rl1v.jpg";
+      mars.current.src="https://as2.ftcdn.net/v2/jpg/03/41/22/33/1000_F_341223314_TuiMtFvlAba07hrDA6GX8Hz3Omt5CdZV.jpg";
       function init() {
         window.requestAnimationFrame(draw);
       }
       function draw() {
         ctx.globalCompositeOperation = 'destination-over';
-        ctx.clearRect(0, 0, 300, 300); // clear canvas
+        ctx.clearRect(0, 0, 1000, 1000); // clear canvas
   
         ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
         ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
         ctx.save();
-        ctx.translate(150, 150);
+        ctx.translate(500, 300);
+        ctx.save();
   
         // Earth
         const time = new Date();
@@ -47,27 +51,36 @@ const Home = () => {
           ((2 * Math.PI) / 60) * time.getSeconds() +
             ((2 * Math.PI) / 60000) * time.getMilliseconds(),
         );
-        ctx.translate(105, 0);
+        ctx.translate(250, 0);
         ctx.fillRect(0, -12, 40, 24); // Shadow
-        ctx.drawImage(earth.current, -12, -12,20,20);
+        ctx.drawImage(earth.current, -20, -20,50,50);
+        ctx.restore();
+
+        
   
-        // Moon
-        // ctx.save();
-        // ctx.rotate(
-        //   ((2 * Math.PI) / 6) * time.getSeconds() +
-        //     ((2 * Math.PI) / 6000) * time.getMilliseconds(),
-        // );
-        // ctx.translate(0, 28.5);
-        // ctx.drawImage(moon.current, -3.5, -3.5);
-        // ctx.restore();
+        // Mars
+        ctx.save();
+         const marsTime = new Date();
+         ctx.rotate(
+           ((2 * Math.PI) / 6) * marsTime.getSeconds() +
+             ((2 * Math.PI) / 6000) * marsTime.getMilliseconds(),
+         );
+         ctx.translate(350,0);
+         ctx.drawImage(mars.current, -3.5, -3.5, 50,50);
+         ctx.restore();
   
         ctx.restore();
   
         ctx.beginPath();
-        ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
+        ctx.arc(500, 300, 250, 0, Math.PI * 2, false); // Earth orbit
+        ctx.stroke();
+        ctx.closePath();
+        //mars orbit
+        ctx.beginPath();
+        ctx.arc(500, 300, 350, 0, Math.PI * 2, false); // Mars orbit
         ctx.stroke();
   
-        ctx.drawImage(sun.current, 0, 0, 300, 300);
+        ctx.drawImage(sun.current, 400, 200, 200, 200);
   
         window.requestAnimationFrame(draw);
       }
@@ -81,14 +94,14 @@ const Home = () => {
     return (
       
     <div className="App-header">
-        <canvas ref={canvasref} width="300" height="300" ></canvas>
+        <canvas ref={canvasref} width="1000" height="1000" ></canvas>
         {/*<header className="App-header">*/}
         <img className="logo" src="https://www.ualberta.ca/women-in-scholarship-engineering-science-technology/media-library/images/logos/copy-of-faculty-of-engineering.jpg" style={{width:"300px",height:"100px", position: 'absolute', left:'0', top:'0'}}></img>
         <div className='button-container'>
           <button className='btn btn-outline-success btn-lg' style={{fontFamily:'cursive', border:'none', backgroundColor: 'transparent'}} onClick={toAbout}>About me</button>
           <button className='btn btn-outline-info btn-lg' style={{fontFamily:'cursive',border:'none',backgroundColor: 'transparent'}} onClick={scrolltoBottom}>Contact me</button>
         </div>
-        <iframe className="gif" src="https://giphy.com/embed/qgQUggAC3Pfv687qPC" width="400" height="300" frameBorder="0" class="giphy-embed"></iframe>
+        {/* <iframe className="gif" src="https://giphy.com/embed/qgQUggAC3Pfv687qPC" width="400" height="300" frameBorder="0" class="giphy-embed"></iframe> */}
       {/*Typewriter  onInit={(typewriter)=>{ typewriter.typeString("Hello, I am Harshit Malik").pauseFor(1000).deleteAll().typeString("I am in my final year as a software engineering co-op student at the University of Alberta").start();}}>*/}
       <div className="typewriter">
       <Typewriter
