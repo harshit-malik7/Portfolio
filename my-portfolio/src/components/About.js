df84146caa79ed52cf4e3335fe3bec9cb4b1f5bd
harshit-malik7 <harshit4731@gmail.com>
2024-02-10 16:12:28 -0700

    added skills and updated resume

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Typewriter from 'typewriter-effect';
 import { Typography, colors } from '@mui/material';
 import ListSubheader from '@mui/material/ListSubheader';
@@ -13,7 +13,8 @@ import SendIcon from '@mui/icons-material/Send';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import StarBorder from '@mui/icons-material/StarBorder';
-import Resume_july_2023 from './Resume_july_2023.pdf';
+import Resume_Harshit_Malik_Grad from './Resume_Harshit_Malik_Grad.pdf';
+import Globe from 'react-globe.gl';
  
 
 const About = () => {
@@ -28,6 +29,37 @@ const About = () => {
   const handleClick2 = () => {
     setOpen2(!open2);
   };
+  const labelsData = [
+  {"city": "C++", "lat": 0, "lng": 30},
+  {"city": "Java", "lat": 20, "lng": 60},
+  {"city": "Rust", "lat": 40, "lng": 90},
+  {"city": "JavaScript", "lat": 60, "lng": 120},
+  {"city": "MySQL", "lat": 80, "lng": 150},
+  {"city": "SQLite", "lat": 10, "lng": -60},
+  {"city": "OpenCV", "lat": -60, "lng": -90},
+  {"city": "Reactjs", "lat": 10, "lng": -100},
+  {"city": "Flask", "lat": 20, "lng": -150},
+  {"city": "Python", "lat": 30, "lng": 180},
+  {"city": "ARMv-7", "lat": 40, "lng": -30},
+  {"city": "MATLAB", "lat": -60, "lng": 30},
+  {"city": "HTML", "lat": 10, "lng": 100},
+  {"city": "VMware", "lat": 60, "lng": -90},
+  {"city": "Networking TCP/IP", "lat": 30, "lng": -120},
+  {"city": "AWS", "lat": -40, "lng": 150},
+  {"city": "PowerShell", "lat": 0, "lng": 180},
+  {"city": "Git", "lat": 20, "lng": -30},
+  {"city": "Circuit Designing", "lat": -20, "lng": -60},
+  {"city": "Android Studio", "lat": 40, "lng": 50},
+  {"city": "node.js", "lat": -20, "lng": 100},
+  {"city": "VHDL", "lat": -30, "lng": 120},
+  {"city": "docker", "lat": 60, "lng": -30},
+  {"city": "django", "lat": 0, "lng": 60},
+  {"city": "MongoDB", "lat": -40, "lng": -90},
+  {"city": "Three.js", "lat": 0, "lng": -120},
+  {"city": "Modbus", "lat": 20, "lng": 150},
+  {"city": "REST API", "lat": -20, "lng": 180}
+  ];
+
   return (
     <div className='About-header'>      
       <div className='experience' >
@@ -54,19 +86,22 @@ const About = () => {
               <ListItemIcon color='black'>
                 <StarBorder />
               </ListItemIcon>
-              <ListItemText style={{color:"black"}} primary="Developed a web application using React.js and Node.js to help the company’s clients manage their finances" />
+              <ListItemText style={{color:"black"}} primary="Single-handedly engineered a robust Node.js API interfacing with Modbus servers in PLCs, streamlining 
+data retrieval processes." />
             </ListItemButton>
             <ListItemButton sx={{ pl: 4 }}>
               <ListItemIcon>
                 <StarBorder />
               </ListItemIcon>
-              <ListItemText style={{color:"black"}} primary="Implemented a feature to allow users to upload their financial documents to the application" />
+              <ListItemText style={{color:"black"}} primary="Conceived, designed, and implemented a ReactJS web application to empower clients with remote 
+control over miners, pumps, fans, and HVAC systems, addressing a crucial need for efficient management." />
             </ListItemButton>
             <ListItemButton sx={{ pl: 4 }}>
               <ListItemIcon>
                 <StarBorder />
               </ListItemIcon>
-              <ListItemText style={{color:"black"}} primary="Developed a feature to allow users to view their financial documents in a table format" />
+              <ListItemText style={{color:"black"}} primary=" Ensured seamless integration between the API and the web application and utilized ThreeJS to improve 
+the UI on the web application." />
             </ListItemButton>
           </List>
           </Collapse>
@@ -102,15 +137,18 @@ const About = () => {
           </List>
 
       </div>
+      <div className='skills' style={{backgroundColor:'white'}}>
+      <Globe width = "50%" height = "100%" backgroundColor='black' showGlobe='false'  labelsData={labelsData} labelLat={d => d.lat} labelLng={d => d.lng} labelText={ d => d.city} labelSize={d => 5} labelDotRadius={d => 0.5} labelColor={() => 'white'} labelResolution={2} labelIncludeDot = 'false' />
+      </div>
       <div className='resume'>
       <object
-      data={Resume_july_2023}
+      data={Resume_Harshit_Malik_Grad}
       type="application/pdf"
       width="100%"
-      style={{ height: 'calc(100vh - 43px)' }}
+      style={{ height: 'calc(100vh - 200px)' }}
       aria-label="This object displays an PDF file"
     />
-        <a href={Resume_july_2023} download="Resume-Harshit_Malik" target="_blank" rel="noreferrer">Download Resume</a>
+        <a href={Resume_Harshit_Malik_Grad} download="Resume-Harshit_Malik" target="_blank" rel="noreferrer">Download Resume</a>
       </div>
     </div>
 
